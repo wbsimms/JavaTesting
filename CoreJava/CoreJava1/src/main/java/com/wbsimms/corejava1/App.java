294e0c8a79c7901d455c8d43a5ced3fd8248c4b0
Barrett Simms <wbsimms@gmail.com>
2013-04-28 20:15:43 -0400

    minor changes

--- CoreJava/CoreJava1/src/main/java/com/wbsimms/corejava1/App.java
+++ CoreJava/CoreJava1/src/main/java/com/wbsimms/corejava1/App.java
@@ -1,5 +1,7 @@
 package com.wbsimms.corejava1;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Scanner s = new Scanner(System.in);
+        String line = s.nextLine();
+        System.out.format("%s %s","You said : ",line);
     }
 }
